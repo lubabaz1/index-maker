8ce19750da5f2225553aa9e60c8948f29470fd0e
Ben Stein <ben.s.stein@gmail.com>
2021-12-13 09:45:48 -0500

    Update index generation code

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
 import json
-import os, os.path
+import os
 from whoosh import index
-from whoosh.fields import Schema, TEXT, ID
+from whoosh.fields import Schema, TEXT, ID, STORED
 from whoosh.analysis import StemmingAnalyzer
 
 index_dir = 'index'
-preprocessed_events = []
+raw_events = []
 
-with open('data_v3.json', 'r') as file:
+with open('data/data_v3.json', 'r') as file:
     file_content = file.readline()
-    json_data = json.loads(file_content)
+    raw_events = json.loads(file_content)
     
-    for event in json_data:
-        preprocessed_events.append({
-            'id': event['id'],
-            'content': event['title'] + event['description']
-        })      
-
 # INDEXING
 if not os.path.exists(index_dir):
     os.mkdir(index_dir)
 
-schema = Schema(doc_no=ID(stored=True),
-            doc_content=TEXT(analyzer=StemmingAnalyzer(), stored=True))
+schema = Schema(
+    doc_no=ID(stored=True),
+    doc_content=TEXT(analyzer=StemmingAnalyzer(), stored=True),
+    title=STORED(),
+    description=STORED(),
+    location=STORED(),
+    date=STORED(),
+    url=STORED(),
+    is_recurring=STORED(),
+    source=STORED()
+)
 
 indexing = index.create_in(index_dir, schema)
 writer = indexing.writer()
 
-for event in preprocessed_events:
-    writer.add_document(doc_no=f'doc{event["id"]}', doc_content=event['content'])
+for event in raw_events:
+    writer.add_document(
+        doc_no=f'doc{event["id"]}', 
+        doc_content=f'${event["title"]} ${event["description"]}',
+        title=event['title'],
+        description=event['description'],
+        location=event['location'],
+        date=event['date'],
+        url=event['link'],
+        is_recurring=event['recurring'] != '',
+        source=event['source']
+    )
 writer.commit()
